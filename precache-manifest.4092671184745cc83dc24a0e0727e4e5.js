04490bcf4a3b80ddb02bd88c347b4d35e615eedf
James Burbage <jeburbage@gmail.com>
2018-11-01 23:51:04 -0600

    Updates

---
+++ precache-manifest.4092671184745cc83dc24a0e0727e4e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc77a0ee3b0cb489f2068047fcfb6736",
+    "url": "/compare_a_country/static/media/globe.bc77a0ee.gif"
+  },
+  {
+    "revision": "f077c7ebb77065a52cd182a98b155fd0",
+    "url": "/compare_a_country/static/media/background.f077c7eb.jpg"
+  },
+  {
+    "revision": "e5b07a174ac91d65100c",
+    "url": "/compare_a_country/static/js/runtime~main.e5b07a17.js"
+  },
+  {
+    "revision": "1d06ffc9d9e9f899f1ed",
+    "url": "/compare_a_country/static/js/main.1d06ffc9.chunk.js"
+  },
+  {
+    "revision": "7cec38b3af9a2cd1755c",
+    "url": "/compare_a_country/static/js/1.7cec38b3.chunk.js"
+  },
+  {
+    "revision": "1d06ffc9d9e9f899f1ed",
+    "url": "/compare_a_country/static/css/main.7e55a43c.chunk.css"
+  },
+  {
+    "revision": "5872c0a62497cde6073ced64edca9a46",
+    "url": "/compare_a_country/index.html"
+  }
+];
